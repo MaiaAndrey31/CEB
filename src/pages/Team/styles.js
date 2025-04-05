309502fcf2dcb953a87acbe26c369afaca03ff2f
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-04-05 15:19:12 -0300

    FontTeamPage

--- src/pages/Team/styles.js
+++ src/pages/Team/styles.js
@@ -10,7 +10,7 @@ flex-direction: column;
 align-items: center;
 
 h1 {
-    font-family: 'Sports-World';
+    font-family: 'Sports World';
     font-size: 3rem;
     color: ${theme.colors.lightblue};
     padding: 1.8rem;
