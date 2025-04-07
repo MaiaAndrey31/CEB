8320f74bf2cde08d46df05d4d1b2d8933977c1d4
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-04-07 17:02:29 -0300

    HeroAdjustHeight

--- src/pages/Hero/styles.js
+++ src/pages/Hero/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { theme } from '../../styles/theme'
 export const Container = styled.div`
   background-color: ${theme.colors.secondary};
-  min-height: 100vh;
+  min-height: 100%;
   width: 100vw;
 `
 
